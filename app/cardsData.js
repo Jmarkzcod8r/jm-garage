0ee4d8a3b05c6ac3bea5de4e8f94d6a550628516
Jmarkzcod8r <106670880+Jmarkzcod8r@users.noreply.github.com>
2025-01-10 04:52:28 +0000

    Added mat.webp

--- app/cardsData.js
+++ app/cardsData.js
@@ -7,18 +7,19 @@ const cards = [
       price: 80,
       stock: 3,
     },
-    // {
-    //   id: 2,
-    //   title: "Linen Blanket",
-    //   description:
-    //     "A luxurious linen blanket perfect for year-round use, it keeps you cool in the summer and cozy in the winter. Its elegant texture and timeless design make it a stylish addition to any bedroom or living space.",
-    //   images: [
-    //     "/pics/linen-blanket.jpg",
+    {
+      id: 2,
+      title: "Exercise Mat",
+      description:
+        "A luxurious linen blanket perfect for year-round use, it keeps you cool in the summer and cozy in the winter. Its elegant texture and timeless design make it a stylish addition to any bedroom or living space.",
+      images: [
+        "/pics/mat.webp",
       
-    //   ], // List of images
-    //   price: 80,
-    //   StocK: 3,
-    // },
+      ], // List of images
+      price: 80,
+      StocK: 3,
+    }
+    // ,
     // {
     //   id: 3,
     //   title: "Car 3",
