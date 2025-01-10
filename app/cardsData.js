3088b5b562cd5c402d24bf2b5a48e24dacf85673
Jmarkzcod8r <106670880+Jmarkzcod8r@users.noreply.github.com>
2025-01-10 07:22:54 +0000

    New changes

--- app/cardsData.js
+++ app/cardsData.js
@@ -1,41 +1,40 @@
 const cards = [
-    {
-      id: 1,
-      title: "Linen Blanket",
-      description: "A versatile and durable mat designed for everyday use.",
-      images: ["/pics/blanket.webp", ], // List of images
-      price: 650,
-      stock: 3,
-      connect: "639305429453",
-    },
-    {
-      id: 2,
-      title: "Exercise Mat",
-      description:
-        "A luxurious linen blanket perfect for year-round use, it keeps you cool in the summer and cozy in the winter. Its elegant texture and timeless design make it a stylish addition to any bedroom or living space.",
-      images: [
-        "/pics/mat.webp",
-      
-      ], // List of images
-      price: 200,
-      stock: 1, connect: "639305429453",
-    }
-    ,
-    {
-      id: 3,
-      title: "Luxxe Protect",
-      description: "Boost your health with Luxxe Protect Pure Grapeseed Extract! Packed with powerful antioxidants, it supports heart, brain, and overall wellness.",
-      images: ["/pics/Protect.webp",], // List of images
-      price: 1000,  stock: 3, connect: "639305429453",
-    },
-    {
-      id: 4,
-      title: "Luxxe Renew.",
-      description: "Revitalize your health with LUXXE RENEW, an all-in-one antioxidant capsule with 8 powerful berries to fight aging, boost immunity, and enhance wellness.",
-      images: ["/pics/Renew2.jpeg",], // List of images
-      price: 2980, stock: 2, connect: "639305429453",
-    }
-  ];
-  
-  export default cards;
-  
+  {
+    id: 1,
+    title: "Linen Blanket",
+    description: "A versatile and durable mat designed for everyday use.",
+    images: ["/pics/blanket.webp", ], // List of images
+    price: 650,
+    stock: 3,
+    connect: "639305429453",
+  },
+  {
+    id: 2,
+    title: "Exercise Mat",
+    description:
+      "A luxurious linen blanket perfect for year-round use, it keeps you cool in the summer and cozy in the winter. Its elegant texture and timeless design make it a stylish addition to any bedroom or living space.",
+    images: [
+      "/pics/mat.webp",
+    
+    ], // List of images
+    price: 200,
+    stock: 1, connect: "639305429453",
+  }
+  ,
+  {
+    id: 3,
+    title: "Luxxe Protect",
+    description: "Boost your health with Luxxe Protect Pure Grapeseed Extract! Packed with powerful antioxidants, it supports heart, brain, and overall wellness.",
+    images: ["/pics/Protect.webp",], // List of images
+    price: 1000,  stock: 3, connect: "639305429453",
+  },
+  {
+    id: 4,
+    title: "Luxxe Renew.",
+    description: "Revitalize your health with LUXXE RENEW, an all-in-one antioxidant capsule with 8 powerful berries to fight aging, boost immunity, and enhance wellness.",
+    images: ["/pics/Luxxe-Renew.jpg",], // List of images
+    price: 2980, stock: 2, connect: "639305429453",
+  }
+];
+
+export default cards;
